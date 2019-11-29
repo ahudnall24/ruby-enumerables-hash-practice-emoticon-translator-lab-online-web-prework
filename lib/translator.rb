905d33aa988926ab7f4cae65fc5a418127e8fa8d
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-29 01:05:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,11 @@ def load_library
 end
 
 def get_japanese_emoticon
-  emoticon=Y# code goes here
+  emoticon=YAML.load_file('emoticon.yml')
+  emoticon
+  # => [":)"]
+  
+  "code goes here
 end
 
 def get_english_meaning
